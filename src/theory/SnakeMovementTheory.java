20c063911ef1ae7a767d49a2de0f3c7a6170d858
Samuel <samugit.projects@gmail.com>
2024-06-15 14:17:14 -0300

    Aula26 A teoria por trás do movimento da snake

---
+++ src/theory/SnakeMovementTheory.java
@@ -0,0 +1,27 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class SnakeMovementTheory {
+
+    /*
+    *
+    * do ponto de vista gráfico cria um novo retângulo para a direita onde é a nova cabeça
+    * do ponto de vista dos dados, que é uma lista, permanece na posição 0 a cabeça e na posição 4 a cauda
+    * com o movimento nos dados pega o que está no anterior e coloca na nova posição, de forma que o 0 o novo Head terá as informações das novas posições
+    *
+    *
+    *               _--_________________
+    *   GRÁFICA {  | T |  |  |  | (H)X | H --> RIGHT
+    *              --------------------  |
+    *                                    |
+    *                 -------------------|
+    *                 |
+    *               v v    v    v    v    v
+    *  DADOS {    | H -> | -> | -> | -> | T |
+    *               0      1    2    3    4
+    *
+    *                   RECT
+    *
+    *
+    * */
+}
